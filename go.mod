4fb1f0ff29858fdf4ca44df9ed4f98710ec17981
leaf <goants@163.com>
2023-11-26 00:03:57 +0800

    xxx

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/goantor/jwt
 
 go 1.20
 
-require github.com/golang-jwt/jwt v3.2.2+incompatible
+require (
+	github.com/goantor/encrypt v0.0.7
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- jwt.go
+++ jwt.go
@@ -2,82 +2,39 @@ package jwt
 
 import (
 	"crypto/ecdsa"
-	"fmt"
 	"github.com/golang-jwt/jwt"
 )
 
-var (
-	Entity = &entityEs256{}
-)
-
-type OptionEs256 struct {
-	PubKeyString string `yaml:"pub_key"`
-	PriKeyString string `yaml:"pri_key"`
-
-	pubKey *ecdsa.PublicKey
-	priKey *ecdsa.PrivateKey
+type IEcdsaOption interface {
+	PubKey() *ecdsa.PublicKey
+	PriKey() *ecdsa.PrivateKey
 }
 
-func (e *OptionEs256) PubKey() *ecdsa.PublicKey {
-	if e.pubKey == nil {
-		var err error
-		e.pubKey, err = jwt.ParseECPublicKeyFromPEM(
-			e.formatKey(e.PubKeyString, true),
-		)
+type IJwt interface {
+	Token(claims jwt.Claims) (string, error)
+	Valid(tokenString string, claims jwt.Claims) (token *jwt.Token, err error)
+}
 
-		if err != nil {
-			panic(err)
-		}
+//type entityEs256 struct {
+//	opt *OptionEs256
+//}
+
+type es256Jwt struct {
+	opt IEcdsaOption
+}
+
+func NewEs256Jwt(opt IEcdsaOption) IJwt {
+	return &es256Jwt{
+		opt: opt,
 	}
-
-	return e.pubKey
 }
 
-func (e *OptionEs256) PriKey() *ecdsa.PrivateKey {
-	if e.priKey == nil {
-		var err error
-		e.priKey, err = jwt.ParseECPrivateKeyFromPEM(
-			e.formatKey(e.PriKeyString, false),
-		)
-
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return e.priKey
-}
-
-func (e *OptionEs256) formatKey(key string, isPublic bool) []byte {
-	var kind string
-	if isPublic {
-		kind = "PUBLIC"
-	} else {
-		kind = "RSA PRIVATE"
-	}
-
-	return []byte(
-		fmt.Sprintf("-----BEGIN %s KEY-----\n%s\n-----END %s KEY-----", kind, key, kind),
-	)
-}
-
-type entityEs256 struct {
-	opt *OptionEs256
-}
-
-func (e *entityEs256) Option(opt *OptionEs256) {
-	opt.PriKey()
-	opt.PubKey()
-
-	e.opt = opt
-}
-
-func (e *entityEs256) Token(claims jwt.Claims) (string, error) {
+func (e *es256Jwt) Token(claims jwt.Claims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
 	return token.SignedString(e.opt.PriKey())
 }
 
-func (e *entityEs256) Valid(tokenString string, claims jwt.Claims) (token *jwt.Token, err error) {
+func (e *es256Jwt) Valid(tokenString string, claims jwt.Claims) (token *jwt.Token, err error) {
 	return jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 		return e.opt.PubKey(), nil
 	})
